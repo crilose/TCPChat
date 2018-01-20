76aa9e8309c5874d0e114357093d125953e2dd0f
Cristiano <Cristiano@FISSO-DEL-CECCA>
2018-01-20 14:08:53 +0100

    Basilare comunicazione chat testuale

--- src/Connection/ServerConnessioneTCP.java
+++ src/Connection/ServerConnessioneTCP.java
@@ -6,9 +6,11 @@
 package Connection;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,20 +30,38 @@ public class ServerConnessioneTCP {
         ServerSocket sSocket = null;
         //oggetto da usare per realizzare la connessione TCP
         Socket connection;
+        //Thread per l'ascolto dei messaggi
+        Listener listen;
+        //Scanner per l'input di una stringa
+        Scanner input;
 
         while(true){
             try{
+                String msg = "";
+                input = new Scanner(System.in);
                 // il server si mette in ascolto sulla porta voluta
                 sSocket = new ServerSocket(port);
                 System.out.println("In attesa di connessioni!");
                 //si è stabilita la connessione
                 connection = sSocket.accept();
+                //Stampo le informazioni sulla connessione
                 System.out.println("Connessione stabilita!");
                 System.out.println("Socket server: " + connection.getLocalSocketAddress());
                 System.out.println("Socket client: " + connection.getRemoteSocketAddress());
+                //OutputStream per l'invio dei dati
+                DataOutputStream dOut = new DataOutputStream(connection.getOutputStream());
                 //InputStream per la ricezione dei dati
                 DataInputStream dIn = new DataInputStream(connection.getInputStream());
-                System.out.println("Messaggio dal client: " + dIn.readUTF());
+                //Istanzio anche il thread listener e lo avvio
+                listen = new Listener(dIn,"Client: ");
+                listen.start();
+                //Mentre la connessione è attiva prendo i messaggi che scrivo e li invio
+                while(connection.isConnected())
+                {
+                    msg = input.nextLine();
+                    dOut.writeUTF(msg);
+                    dOut.flush();
+                }
             }
                catch(IOException e){
                    System.err.println(e);
@@ -53,7 +73,7 @@ public class ServerConnessioneTCP {
             } catch (IOException ex) {
                 System.err.println("Errore nella chiusura della connessione!");
             }
-            System.out.println("Connessione chiusa lato server!");
+            System.out.println("Connessione chiusa lato client!");
         }
       }
 }
