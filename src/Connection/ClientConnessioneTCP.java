2da816bb974c0dc3650a2f5450f214d72ded2862
Cristiano <Cristiano@FISSO-DEL-CECCA>
2018-01-20 14:08:46 +0100

    Basilare comunicazione chat testuale

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -5,6 +5,7 @@
  */
 package Connection;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
@@ -29,18 +30,32 @@ public class ClientConnessioneTCP {
         int port = 2000;
         //Scanner per l'input di una stringa
         Scanner input;
+        //Thread per l'ascolto dei messaggi
+        Listener listen;
 
         //apertura della connessione al server sulla porta specificata
         try{
+            //inizializzo il messaggio che invieremo
+            String msg = "";
+            //Istanzio lo scanner
             input = new Scanner(System.in);
+            //Realizzo la connessione vera e propria tramite ip e porta
             connection = new Socket(serverAddress, port);
+            System.out.println("Connessione aperta");
             //OutputStream per l'invio dei dati
             DataOutputStream dOut = new DataOutputStream(connection.getOutputStream());
-            System.out.println("Connessione aperta");
-            System.out.println("Scrivi il tuo messaggio al server: ");
-            String msg = input.nextLine();
-            dOut.writeUTF(msg);
-            dOut.flush();
+            //InputStream per la ricezione dei dati
+            DataInputStream dIn = new DataInputStream(connection.getInputStream());
+            //Istanzio anche il thread listener e lo avvio
+            listen = new Listener(dIn,"Server: ");
+            listen.start();
+            //Mentre la connessione è attiva prendo i messaggi che scrivo e li invio
+            while(connection.isConnected())
+            {
+                msg = input.nextLine();
+                dOut.writeUTF(msg);
+                dOut.flush();
+            }
             
         }
         catch(ConnectException e){
@@ -61,7 +76,7 @@ public class ClientConnessioneTCP {
             if (connection!=null)
                 {
                     connection.close();
-                    System.out.println("Connessione chiusa lato client!");
+                    System.out.println("Connessione chiusa lato server!");
                 }
             }
             catch(IOException e){
