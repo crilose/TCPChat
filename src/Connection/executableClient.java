ec70f9dd5227098ee4e58f906741d615c4ba5219
Cristiano <Cristiano@FISSO-DEL-CECCA>
2018-01-23 16:36:03 +0100

    Classi runnable di client e server

---
+++ src/Connection/executableClient.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+/**
+ *
+ * @author Cristiano
+ */
+public class executableClient {
+    
+    public static void main(String[] args) {
+        
+        ClientConnessioneTCP client = new ClientConnessioneTCP();
+        client.startConnection();
+        client.setUsername();
+        while(true){
+            client.communicate();
+            client.closeConnection();
+        }
+      }
+    
+}
